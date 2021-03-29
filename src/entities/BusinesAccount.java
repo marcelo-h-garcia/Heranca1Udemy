a9d479deb393273dfaf145480a456ddb52053072
Marcelo Helio Garcia <marceloph2@yahoo.com.br>
2021-03-29 08:47:58 -0300

    Herança

---
+++ src/entities/BusinesAccount.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class BusinesAccount extends Account {
+	private Double loanLimit;
+	
+	public BusinesAccount() {
+		super();
+	}
+
+	public BusinesAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if(amount <= loanLimit) {
+		deposit(amount);
+		}
+	}
+	
+}
